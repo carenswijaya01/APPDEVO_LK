f100b421e1ac0bcd10fecfd56efb3b7359ff11e4
Stevanus Pangau <stevanuspangau50@gmail.com>
2022-07-30 14:02:44 +0700

    tes commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,7 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+                
             }
         </style>
     </head>
@@ -44,6 +45,7 @@
                         </g>
                     </svg>
                 </div>
+                <!--  -->
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
