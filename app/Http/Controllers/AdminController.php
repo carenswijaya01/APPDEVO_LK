8b371ebc3425a717f913737aeb11b8e89347f250
YoasAriel <yoas7053@gmail.com>
2022-08-12 14:53:20 +0700

    Crud admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $admin = Admin::paginate(10);
+        return view('dataadmin', compact('admin'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'nim'     => 'required',
+            'name'     => 'required',
+            'email'     => 'required',
+            'password'   => 'required | confirmed'
+        ]);
+    
+        $admin = Admin::create($data + ['role'     =>  'admin',]);
+    
+        if($admin){
+            //redirect dengan pesan sukses
+            return redirect()->back()->with(['success' => 'Data Berhasil Disimpan!']);
+        }else{
+            //redirect dengan pesan error
+            return redirect()->back()->with(['error' => 'Data Gagal Disimpan!']);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Admin $admin)
+    {
+        return view('edit', compact('admin'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Admin $admin)
+    {
+        $this->validate($request,[
+            'nim' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $admin->update([
+            'nim' => $request->nim,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+        return redirect()->back()->with('success','Data berhasil diubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Admin $admin)
+    {
+        $admin->delete();
+        return redirect()->back()->with(['success','Data berhasil dihapus!']);
+    }
+}
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,52 @@
+<?php
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CRUD Admin</title>
+</head>
+<body>
+    <h1> Create Admin Account</h1>
+    <ul>
+        <form action="{{ route('admin.store') }}" method="POST">
+            @csrf
+            <label for="nim">NIM</label> 
+            <input type="text" name="nim" id="nim" placeholder="Enter your NIM" required >
+            <br>
+            <label for="name">Nama</label> 
+            <input type="text" name="name" id="name" placeholder="Enter your name" required>
+            <br>
+            <label for="email">Email</label>  
+            <input type="email" name="email" id="email" placeholder="Enter your email address" required>
+            <br>    
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" required>
+            <br>
+            <label for="password2">Konfirmasi Password</label>
+            <input type="password" name="password_confirmation" id="password2" placeholder="Confirm your password" required>
+            <br>
+            <input type="submit" name="submit" value="Buat Akun">
+        </form>
+        <form action="{{ route('admin.index') }}" method="GET">
+            @csrf
+            @method('put')
+            <input type="submit" name="lihatdata" value="Lihat Data">
+            </form>
+            <br><br><br>
+
+    
+    @if($errors->any())
+        @foreach($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    @endif
+    @if (session('success'))
+        <p>{{session('success')}}</p> 
+    @endif
+</body>
+</html>
+
+
