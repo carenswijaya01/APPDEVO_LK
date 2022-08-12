64c0d5b98ed86a86650181efaa019f15cc5077f8
georgionicky <nickyr886@gmail.com>
2022-08-12 16:40:54 +0700

    Kegiatan - Georgio

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-<<<<<<< HEAD
+
 use App\Http\Controllers\KegiatanController;
-=======
+
 use App\Http\Controllers\AdminController;
->>>>>>> 5741d0ab45b682c3d666f870faa9979eb795d8ad
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,15 +21,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 
+// Kegiatan
 Route::get('/daftarKegiatan', '\App\Http\Controllers\KegiatanController@index')->name('daftarKegiatan');
 Route::get('/tambahKegiatan', '\App\Http\Controllers\KegiatanController@create')->name('tambahKegiatan');
 Route::post('/simpanKegiatan', '\App\Http\Controllers\KegiatanController@store')->name('simpanKegiatan');
 Route::get('/editKegiatan/{id}', '\App\Http\Controllers\KegiatanController@edit')->name('editKegiatan');
 Route::post('/updateKegiatan/{id}', '\App\Http\Controllers\KegiatanController@update')->name('updateKegiatan');
 Route::get('/deleteKegiatan/{id}', '\App\Http\Controllers\KegiatanController@destroy')->name('deleteKegiatan');
-=======
+
 Route::resource('admin', AdminController::class);
->>>>>>> 5741d0ab45b682c3d666f870faa9979eb795d8ad
