36daceab1b4dad69d8a92d787c8fdcebaee99daa
RHW48 <90231031+RHW48@users.noreply.github.com>
2022-08-15 21:59:59 +0700

    update pop up

--- resources/views/admin/listKepanitiaan.blade.php
+++ resources/views/admin/listKepanitiaan.blade.php
@@ -25,6 +25,8 @@
     </style>
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" type="text/css" href="css/trix.css">
+    <script type="text/javascript" src="js/trix.js"></script>
 </head>
 
 <body>
@@ -82,6 +84,9 @@
                     <h2 text align="center">List Kegiatan Kepanitiaan</h2>
                     <table class="table table-striped text-center table-bordered border-dark table table-hover">
                         <thead class="table-primary table-bordered border-dark">
+                            <tr class="tab">
+                                <th class=""><button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal" id="tambahData"><i class="far fa-plus"></i></button></th>
+                            </tr>
                             <tr>
                                 <th>No.</th>
                                 <th>Nama Kegiatan</th>
@@ -94,50 +99,100 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <th>1</th>
-                                <th>Kegiatan 1</th>
-                                <th>Universitas</th>
-                                <th>James</th>
-                                <th>30 Januari 2023</th>
-                                <th></th>
-                                <th> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
+                                <td>1</td>
+                                <td>Kegiatan 1</td>
+                                <td>Universitas</td>
+                                <td>James</td>
+                                <td>30 Januari 2023</td>
+                                <td></td>
+                                <td> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
                                     <button onclick="myFunction()" type="button" class="btn btn-sm btn-outline-dark">&nbsp;Hapus</button>
-                                </th>
+                                </td>
                             </tr>
                             <tr>
-                                <th>2</th>
-                                <th>Kegiatan 2</th>
-                                <th>Fakultas</th>
-                                <th>Maria</th>
-                                <th>30 Januari 2023</th>
-                                <th></th>
-                                <th> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
+                                <td>2</td>
+                                <td>Kegiatan 2</td>
+                                <td>Fakultas</td>
+                                <td>Maria</td>
+                                <td>30 Januari 2023</td>
+                                <td></td>
+                                <td> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
                                     <button onclick="myFunction()" type="button" class="btn btn-sm btn-outline-dark">&nbsp;Hapus</button>
-                                </th>
+                                </td>
                             </tr>
                             <tr>
-                                <th>3</th>
-                                <th>Kegiatan 3</th>
-                                <th>Progdi</th>
-                                <th>Kevin</th>
-                                <th>30 Januari 2023</th>
-                                <th></th>
-                                <th> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
+                                <td>3</td>
+                                <td>Kegiatan 3</td>
+                                <td>Progdi</td>
+                                <td>Kevin</td>
+                                <td>30 Januari 2023</td>
+                                <td></td>
+                                <td> <button type="button" class="btn btn-sm btn-outline-dark">&nbsp;Edit</button>
                                     <button onclick="myFunction()" type="button" class="btn btn-sm btn-outline-dark">&nbsp;Hapus</button>
-                                </th>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
-
-            <div class="button">
-                <a href="admin-kepanitiaan"><input type="submit" value="Tambah"></a>
+        <div class="container mt-5">
+        <div class="modal" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Input Poin</h5>
+                        <button type="button" class="btn-close btn btn-light" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body">
+                            <form action="/action_page.php">
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Nama Kegiatan</label>
+                                    <input type="text" class="form-control" id="name" name="namaacara" placeholder="Nama Kegiatan" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="aras" class="form-label">Tanggal Pelaksanaan</label>
+                                    <input type="date" class="form-control" placeholder="tanggal upload" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="aras" class="form-label">Tingkat Kegiatan</label>
+                                    <select id="aras" name="aras" class="form-select">
+                                        <option value="universitas">Universitas</option>
+                                        <option value="fakultas">Fakultas</option>
+                                        <option value="progdi">Progdi</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="kategori" class="form-label">Ketua Kegiatan</label>
+                                    <input type="text" class="form-control" id="ketua" name="ketua" placeholder="Nama - NIM" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="image" class="form-label">Gambar</label>
+                                    <input required type="file" accept="image/png, image/jpg" name="upload_file" class="form-control" id="upload_file" onchange="getImagePreview(event)">
+                                    <center>
+                                        <div id="preview">
+                                        </div>
+                                    </center>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="keterangan" class="form-label">Deskripsi Kegiatan</label>
+                                    <trix-editor input="Isi"></trix-editor><br />
+                                </div>
+                                </form>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-dark">Simpan</button>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-            <script src="js/script.js"></script>
+     
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 
 </html>
